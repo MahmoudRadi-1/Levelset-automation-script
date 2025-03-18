20dc2a103f5b4be403952c244529a6de76759c93
MahmoudRadi-1 <mahmoudradi3815@gmail.com>
2025-03-18 12:15:45 +0200

    Update ActionsHandler.java

--- Levelset/src/actions/java/org.example/ActionsHandler.java
+++ Levelset/src/actions/java/org.example/ActionsHandler.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions; // Import Actions for double-click
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
